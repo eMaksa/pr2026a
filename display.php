c203633c129977208b62271888341e718a24858a
Jegors Kudirka <Jegors.Kudirka@dt.local>
2026-01-19 08:50:43 +0000

    lala

--- display.php
+++ display.php
@@ -2,9 +2,9 @@
 
 <table>
     <tr>
-        <th>ID</th>
-        <th>Username</th>
-        <th>Email</th>
+        <th onclick="sortTable('id')" style="cursor:pointer">ID ▲▼</th>
+        <th onclick="sortTable('username')" style="cursor:pointer">Username ▲▼</th>
+        <th onclick="sortTable('email')" style="cursor:pointer">Email ▲▼</th>
         <th>Edit</th>
     </tr>
     <tbody id="usersBody">
