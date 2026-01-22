51fe8ef8e834a30d0590ec26bfe921b35c6880a8
Jegors Kudirka <okosov25543@gmail.com>
2026-01-22 10:37:53 +0200

    actual_schema

--- update_003_database.sql
+++ update_003_database.sql
@@ -1,3 +1,13 @@
+CREATE TABLE users_audit (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    old_email VARCHAR(255), new_email VARCHAR(255),
+    changed_at DATETIME NOT NULL,
+    changed_by VARCHAR(45), -- IP (IPv4 / IPv6)
+    INDEX (user_id) 
+    );
+
+
 DELIMITER $$
 
 CREATE TRIGGER users_email_audit
@@ -16,7 +26,7 @@ BEGIN
             OLD.email,
             NEW.email,
             NOW(),
-            @changed_by
+            COALESCE(@changed_by, 'unknown')
         );
     END IF;
 END$$
